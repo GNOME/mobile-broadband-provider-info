b69f9697b32e7b2aa4b1296c73db888953606698
Antti Kaijanmäki <kaijanma@src.gnome.org>
2008-08-14 07:49:29 +0000

    add linebreak after file list
    svn path=/trunk/; revision=9

--- generate_cl.sh
+++ generate_cl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-svn2cl --group-by-day --authors=authors.svn2cl http://svn.gnome.org/svn/mobile-broadband-provider-info/trunk
+svn2cl --group-by-day --break-before-msg --authors=authors.svn2cl http://svn.gnome.org/svn/mobile-broadband-provider-info/trunk
