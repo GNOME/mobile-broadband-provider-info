e5fd0d73061cd10e89d0f5a8c0e748312653ce65
Dan Williams <dcbw@redhat.com>
2009-05-26 23:18:20 -0400

    add an autogen.sh; clean up configure.ac

---
+++ autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+REQUIRED_AUTOMAKE_VERSION=1.7
+PKG_NAME=mobile-broadband-provider-info
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/serviceproviders.xml) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+
+(cd $srcdir;
+    autoreconf --install --symlink &&
+    autoreconf &&
+    ./configure --enable-maintainer-mode $@
+)
+
